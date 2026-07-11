bc5c1e90d4c24f87c5093d362a2e6871dfe7af1f
amrxtgh <ghimireamrit619@gmail.com>
2026-07-11 12:27:19 +0545

    refactor: fix enum state to struct enum and also add the on_packet function that is used by the connections with connections.entry()

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,5 +1,25 @@
-pub enum State {
-    Listen,
-    SynRcvd,
-    Estab,
+pub struct State {}
+
+impl Default for State {
+    fn default() -> Self {
+        State {}
+    }
+}
+
+impl State {
+    pub fn on_packet<'a>(
+        &mut self,
+        ipv4h: etherparse::Ipv4HeaderSlice<'a>,
+        tcph: etherparse::TcpHeaderSlice<'a>,
+        data: &'a [u8],
+    ) {
+        eprintln!(
+            "{}: {} -> {}:{} {}b of tcp",
+            ipv4h.source_addr(),
+            tcph.source_port(),
+            ipv4h.destination_addr(),
+            tcph.destination_port(),
+            data.len(),
+        )
+    }
 }
